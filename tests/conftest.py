01cabe47aa357982ef2797d4c7f214601e336947
Mikhail Stepanov <m.stepanov@insilicomedicine.com>
2021-12-27 19:58:23 +0300

    fix fixture

--- tests/conftest.py
+++
@@ -1,5 +0,0 @@
-import pytest
-
-@pytest.fixture
-def fixture_template():
-    return "Hello World"
--- tests/test_temlate.py
+++ tests/test_temlate.py
@@ -1,5 +1,5 @@
 from py_template.template import hello_world
 
 
-def test_template(fixture_template):
-    assert hello_world() == fixture_template
+def test_template():
+    assert hello_world() == "Hello World!"
